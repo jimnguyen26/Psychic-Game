2f36f05f225b2c0f82735c0ce295b3fb807d644b
Jim <jimmybnguyen@yahoo.com>
2019-10-02 23:52:09 -0700

    html update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,13 @@
-console.log('hello');
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var winCount = 1;
+var lossCount = 0;
+var guessesRemaining = 10;
+var guessedLetters = [];
 
-document.onkeyup = function () {
-    alert("working!");
+var randomIndex = Math.floor(Math.random() * alphabet.length);
+var computerChoice = alphabet[randomIndex];
+console.log(computerChoice);
+
+for (var i = 0; i < alphabet.length; i++) {
+    console.log(alphabet[i]);
 }
