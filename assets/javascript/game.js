4fc8e7a087277a01a1a9b65b2ce271b5796754ae
Jim <jimmybnguyen@yahoo.com>
2019-10-04 15:26:55 -0700

    half way there

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,12 @@
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var winCount = 1;
+var winCount = 0;
 var lossCount = 0;
 var guessesRemaining = 10;
 var guessedLetters = [];
+
+var winsText = document.getElementById("#char1");
+var lossText = document.getElementById('#char2');
+var guessesText = document.getElementById('#char3');
 
 var randomIndex = Math.floor(Math.random() * alphabet.length);
 var computerChoice = alphabet[randomIndex];
@@ -13,8 +17,43 @@ console.log(computerChoice);
 // }
 
 document.onkeyup = function () {
-    var userguess = String.fromCharCode(event.keyCode).toLowerCase();
-    console.log(userguess);
 
-    userguess === computerChoice ? alert('winner') : console.log('try again');
-}
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    console.log(userGuess);
+    document.querySelector('#char4').textContent = event.key;
+
+    if ((userGuess === computerChoice)) {
+        winCount++;
+        alert("You win!");
+    } else if (userGuess !== computerChoice)
+
+        guessesRemaining--;
+
+    // else((userGuess!--computerChoice)) {
+    // guessesRemaining--;
+    // rr}
+
+
+
+
+
+
+
+
+    winsText.textContent = winCount;
+    lossText.textContent = lossCount;
+    guessesText.textContent = guessesRemaining;
+
+
+};
+// userGuess === computerChoice ? alert('Correct guess!') : console.log('try again');
+
+
+// var count = 0;
+// document.onkeyup = function (event) {
+//     if (event.keyCode === 13) {
+//         count++;
+//         console.log('hit:', count, 'times.');
+//     }
+//     document.querySelector('#char4').textContent = event.key;
+// }
