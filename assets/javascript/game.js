7d3e19e0d8eb927b646f21b489a4e3e4a2c961a8
Jim <jimmybnguyen@yahoo.com>
2019-10-04 20:14:51 -0700

    how to append guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,20 @@
-var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+    "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+];
+
 var winCount = 0;
 var lossCount = 0;
 var guessesRemaining = 10;
-var guessedLetters = [];
 
 
 var winsText = document.getElementById("#char1");
 var lossText = document.getElementById('#char2');
 var guessesText = document.getElementById('#char3');
 
-var randomIndex = Math.floor(Math.random() * alphabet.length);
-var computerChoice = alphabet[randomIndex];
-console.log(computerChoice);
 
-// for (var i = 0; i < alphabet.length; i++) {
-//     console.log(alphabet[i]);
-// }
+var randomIndex = Math.floor(Math.random() * abc.length);
+var computerChoice = abc[randomIndex];
+console.log(computerChoice);
 
 document.onkeyup = function () {
 
@@ -23,40 +22,30 @@ document.onkeyup = function () {
     console.log(userGuess);
     document.querySelector('#char4').textContent = event.key;
 
+
     if ((userGuess === computerChoice)) {
         winCount++;
         alert("You win!");
-
+        randomIndex = Math.floor(Math.random() * abc.length);
+        computerChoice = abc[randomIndex];
+        console.log(computerChoice);
+        guessesRemaining = 10;
 
     } else if ((userGuess !== computerChoice)) {
         guessesRemaining--;
 
+
     }
     if (guessesRemaining === 0) {
         lossCount++;
-
+        alert("You lose!");
+        randomIndex = Math.floor(Math.random() * abc.length);
+        computerChoice = abc[randomIndex];
+        console.log(computerChoice);
+        guessesRemaining = 10;
     }
-
-
-    // else((userGuess!--computerChoice)) {
-    // guessesRemaining--;
-    // rr}
 
     winsText.textContent = winCount;
     lossText.textContent = lossCount;
     guessesText.textContent = guessesRemaining;
-
 };
-
-
-// userGuess === computerChoice ? alert('Correct guess!') : console.log('try again');
-
-
-// var count = 0;
-// document.onkeyup = function (event) {
-//     if (event.keyCode === 13) {
-//         count++;
-//         console.log('hit:', count, 'times.');
-//     }
-//     document.querySelector('#char4').textContent = event.key;
-// }
