f7c24030d0d0cf600ff907b8cccbd53134106ca8
Jim <jimmybnguyen@yahoo.com>
2019-10-05 07:09:41 -0700

    pushing what i have left

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,12 +5,11 @@ var abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
 var winCount = 0;
 var lossCount = 0;
 var guessesRemaining = 10;
-
+var guessedLetters = [];
 
 var winsText = document.getElementById("#char1");
 var lossText = document.getElementById('#char2');
 var guessesText = document.getElementById('#char3');
-
 
 var randomIndex = Math.floor(Math.random() * abc.length);
 var computerChoice = abc[randomIndex];
@@ -22,7 +21,6 @@ document.onkeyup = function () {
     console.log(userGuess);
     document.querySelector('#char4').textContent = event.key;
 
-
     if ((userGuess === computerChoice)) {
         winCount++;
         alert("You win!");
@@ -33,9 +31,8 @@ document.onkeyup = function () {
 
     } else if ((userGuess !== computerChoice)) {
         guessesRemaining--;
-
-
     }
+
     if (guessesRemaining === 0) {
         lossCount++;
         alert("You lose!");
